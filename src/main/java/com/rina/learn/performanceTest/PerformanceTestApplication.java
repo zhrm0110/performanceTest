84ff479014567d8d0a06505d5c284e9a7b08c809
Rina Zhao <rina.zhao@sap.com>
2018-06-29 17:18:37 +0800

    init

---
+++ src/main/java/com/rina/learn/performanceTest/PerformanceTestApplication.java
@@ -0,0 +1,12 @@
+package com.rina.learn.performanceTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rina/learn/performanceTest/PerformanceTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rina.learn.performanceTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PerformanceTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
